b7bc09563a34979fe9556cb56d8b1b267504bae1
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-29 00:01:29 -0600

    started skills component

---
+++ src/components/skills/Skills.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <section className="skills section" id="skills">
+      <h2 className="section__title">Skills</h2>
+
+<span className="section__subtitle">Technical Level</span>
+    </section>
+  )
+}
+
+export default Skills
