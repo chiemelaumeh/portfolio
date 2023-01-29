473f07ff167a7d4f6f0c7853659729035d1ce6fd
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-28 18:26:17 -0600

    added classes to navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,16 @@ const Header = () => {
     <header className="header">
       <nav className="nav container">
         <a href="index.html" className="nav__logo">Chiemela</a>
-        <div className="nav__menu"></div>
+        <div className="nav__menu">
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="" className="nav__link">
+                <i className="uil uil- nav__icon"></i>
+              </a>
+            </li>
+          </ul>
+
+        </div>
       </nav>
     </header>
   );
