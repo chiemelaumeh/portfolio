d3a70222fc6d92cc858b6f308e3e1d1739d80db1
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-29 03:09:21 -0600

    imported data for testimonial

--- src/components/testimonials/Testimonial.js
+++ src/components/testimonials/Testimonial.js
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import "./testimonial.css";
+import { Data } from "./Data";
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
-  )
-}
+    <section className="testimonial container section">
+      <h2 className="section__title">"My clients say"</h2>
+      <span className="section_subtitle">Testimonial</span>
+      <div className="testimonial_container">
+        {Data.map(({id, image, title, description}) => {
 
-export default Testimonial
+          return (
+          <div className="testimonial__card" key={id}>
+            <img src={image} alt="" />
+
+
+          </div>
+          )
+         
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Testimonial;
