0e935b6dba3be5bca5957ef760fd85c5ce21aa81
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-28 18:23:56 -0600

    created nav bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import "./header.css";
 
 const Header = () => {
   return (
-    <div>Real Header</div>
-  )
-}
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">Chiemela</a>
+        <div className="nav__menu"></div>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
