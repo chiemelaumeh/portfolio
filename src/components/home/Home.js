5261473ca170c67e58d69b968ea6396544c93e49
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-28 21:32:10 -0600

    added mouse up btn

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./home.css";
 import Social from "./Social";
 import Data from "./Data";
+import ScrollDown from "./ScrollDown";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
           <div className="home__img"></div>
           <Data />
         </div>
+        <ScrollDown />
       </div>
     </section>
   );
---
+++ src/components/home/ScrollDown.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ScrollDown = () => {
+  return (
+    <div className="home__scroll">
+    <a href="#about" className="home__scroll-button button--flex">
+    <i class="uil uil-mouse-alt"></i>
+    </a>
+    </div>
+  )
+}
+
+export default ScrollDown
