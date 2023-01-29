affd8e42dd3401acefeb5c91e424bde863e87958
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-29 03:45:22 -0600

    created testimonial section

--- src/components/testimonials/Testimonial.js
+++ src/components/testimonials/Testimonial.js
@@ -12,32 +12,29 @@ import "swiper/css/pagination";
 // import required modules
 import { Pagination } from "swiper";
 
-
-
 const Testimonial = () => {
   return (
     <section className="testimonial container section">
       <h2 className="section__title">"My clients say"</h2>
       <span className="section__subtitle">Testimonial</span>
-      <Swiper className="testimonial_container"
-             loop={true}
-             spaceBetween={24}
-             pagination={{
-               clickable: true,
-             }}
-             breakpoints={{
-               576: {
-                 slidesPerView: 2,
-               
-               },
-               768: {
-                 slidesPerView: 2,
-                 spaceBetween: 48,
-               },
-          
-             }}
-             modules={[Pagination]}
-            
+      <Swiper
+        className="testimonial_container"
+        loop={true}
+        grabCursor={true}
+        spaceBetween={24}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          576: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+          },
+        }}
+        modules={[Pagination]}
       >
         {Data.map(({ id, image, title, description }) => {
           return (
