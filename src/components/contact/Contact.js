c0ae897fe9f9372a9e721af00cf2b940b77e7e4b
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-29 04:09:28 -0600

    building contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -11,30 +11,44 @@ const Contact = () => {
           <h3 className="contact__title">Talk to me</h3>
           <div className="contact__info">
             <div className="contact__card">
-              <i className="bx bx contact__card-icon"></i>
+              <i className="bx bx-mail-send contact__card-icon"></i>
               <h3 className="contact__card-title">Email</h3>
-              <span className="contact__card-data">user2gmail.com</span>
-              <a href="mailto:franklyumeh06@gmail.com" className="contact__button">
+              <span className="contact__card-data">
+                franklynumeh06gmail.com{" "}
+              </span>
+              <a
+                href="mailto:franklyumeh06@gmail.com"
+                className="contact__button"
+              >
                 Write me
                 <i className="bx bx-right-arrow-alt contsct__button-icon"></i>
               </a>
             </div>
 
             <div className="contact__card">
-              <i className="bx bx contact__card-icon"></i>
+              <i className="bx bx-phone contact__card-icon"></i>
               <h3 className="contact__card-title">Mobile</h3>
-              <span className="contact__card-data">23456789</span>
+              <span className="contact__card-data">(508)-825 6171 </span>
               <a href="" className="contact__button">
-                Write me
+                Call me
                 <i className="bx bx-right-arrow-alt contsct__button-icon"></i>
               </a>
             </div>
-
-
           </div>
         </div>
         <div className="contact__content">
           <h3 className="contact__title">Write me you project</h3>
+
+          <form className="contact__form"></form>
+          <div className="contact__form-div">
+            <label className="contact__form-tag">Name</label>
+            <input
+              type="text"
+              name="name"
+              className="contact__form-input"
+              placeholder="Enter your name"
+            />
+          </div>
         </div>
       </div>
     </section>
