97fa76e1212c0e47ea56e2870d084bacad0ee530
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-28 19:48:31 -0600

    added social icons

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
+import "./home.css"
+import Social from "./Social";
 
 const Home = () => {
   return (
     <section className="home section" id="home">
       <div className="home__container container grid">
         <div className="home__content grid">
+          <Social />
           
+
         </div>
       </div>
     </section>
--- src/components/home/Social.js
+++ src/components/home/Social.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Social = () => {
   return (
-    <div className="homee__social">
-      <a href="" className="home__social-icon" target="_blank">
-      <i class="uil uil-instagram"></i>
-        
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/blackflame_6/?hl=en"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram"></i>
       </a>
-      <a href="" className="home__social-icon" target="_blank">
-
+      <a
+        href="https://reddit-app-nw97.onrender.com/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-dribbble"></i>
       </a>
-      <a href="" className="home__social-icon" target="_blank">
-
+      <a
+        href="https://github.com/Blackflame6"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
       </a>
     </div>
-  )
-}
+  );
+};
 
-export default Social
+export default Social;
