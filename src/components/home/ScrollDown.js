eb153567aa41f89a6f3106978eee64a78a71b1a8
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-28 22:09:53 -0600

    adding media queries for medium screens

--- src/components/home/ScrollDown.js
+++ src/components/home/ScrollDown.js
@@ -5,6 +5,8 @@ const ScrollDown = () => {
     <div className="home__scroll">
     <a href="#about" className="home__scroll-button button--flex">
     <i class="uil uil-mouse-alt"></i>
+    <span className="home__scroll-name">Scroll Down</span>
+    <i class="uil uil-arrow-down home__scroll-arr"></i>
     </a>
     </div>
   )
