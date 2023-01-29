b3dff113aacbb750d187b87b591fc8f7b0ff8c5d
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-28 22:50:01 -0600

    building info section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -12,16 +12,19 @@ const About = () => {
       <span className="section__subtitle">My introduction</span>
       <div className="about__container container grid">
         <img src={AboutImg} alt="" className="about__img" />
-        <div className="about__data"></div>
-        <Info />
-        <p className="about__description">
-          Enterprising, self-motivated and teachable technology professional
-          keen on leveraging full stack web development skills to build,
-          maintain, and distribute computer software. Versatile problem-solver
-          with strong interpersonal, communication, customer service, and design
-          skills. Deep understanding of object-oriented programming and
-          test-driven deployment.
-        </p>
+        <div className="about__data">
+          <Info />
+          <p className="about__description">
+            Enterprising, self-motivated and teachable technology professional
+            keen on leveraging full stack web development skills to build,
+            maintain, and distribute computer software. Versatile problem-solver
+            with strong interpersonal, communication, customer service, and
+            design skills. Deep understanding of object-oriented programming and
+            test-driven deployment.
+          </p>
+          <a download="" href={Resume} className="button button--flex">Download Resume <i class="uil uil-file-download-alt"></i></a>
+          
+        </div>
       </div>
     </section>
   );
--- src/components/about/Info.js
+++ src/components/about/Info.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Info = () => {
   return (
-    <div>info</div>
-  )
-}
+    <div className="about__info grid">
+      <div className="about__box">
+      <i class='bx bx-award'></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">1+ Years Working</span>
+      </div>
 
-export default Info
+      <div className="about__box">
+      <i class='bx bx-briefcase-alt'></i>
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">5 Projects</span>
+      </div>
+
+      <div className="about__box">
+      <i class='bx bx-support'></i>
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">Online 24/7</span>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
