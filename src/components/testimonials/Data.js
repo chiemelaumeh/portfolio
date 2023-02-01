7873ce0a9bba3b09daab681dd9f3b18de156a608
Blackflame6 <engineerfranklyn@gmail.com>
2023-01-31 18:19:32 -0600

    ready to deploy

--- src/components/testimonials/Data.js
+++ src/components/testimonials/Data.js
@@ -1,6 +1,6 @@
-import Image1 from "../../assets/testimonial1.png"
-import Image2 from "../../assets/testimonial2.png"
-import Image3 from "../../assets/testimonial3.png"
+import Image1 from "../../assets/test2.png"
+import Image2 from "../../assets/test1.png"
+import Image3 from "../../assets/test.png"
 
 
 
@@ -8,10 +8,12 @@ export const Data = [
   {
     id: 1,
     image: Image1,
-    title: "Athena",
+  
+    title: "Reddit",
     description:
       "A really good job, all aspects of the project were followed step by step and with good results.",
-      link: "https://ticket-manager-app.onrender.com/"
+     
+      link:"https://reddit-app-nw97.onrender.com/"
   },
   {
     id: 2,
@@ -19,13 +21,15 @@ export const Data = [
     title: "Dreams Come Brew",
     description:
       "A really good job, all aspects of the project were followed step by step and with good results.",
-      link:"https://reddit-app-nw97.onrender.com/"
+      link: "https://dreamscomebrew.onrender.com/"
+     
   },
   {
     id: 3,
     image: Image3,
-    title: "Reddit",
+    title: "Athena",
     description:
       "A really good job, all aspects of the project were followed step by step and with good results.",
+      link: "https://ticket-manager-app.onrender.com/"
   },
 ];
