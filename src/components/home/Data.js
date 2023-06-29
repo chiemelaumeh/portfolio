63105bb94169641066099df3dee7cdfdd5025c32
Blackflame6 <engineerfranklyn@gmail.com>
2023-06-29 05:50:30 -0500

    update build for gh pages

--- src/components/home/Data.js
+++ src/components/home/Data.js
@@ -6,7 +6,7 @@ const Data = () => {
       <h1 className="home__title">Chiemela Umeh</h1>
       <h3 className="home__subtitle"> Full Stack Developer</h3>
       <p className="home__description">
-        Hey! Thank you for visiting my website. I am Chiemela, a U.S Army veteran and full stack developer. I am proficient with MERN stack, however, I learn different technologies and add them to my skill set. Outside of work, I enjoy playing chess, working out, and attending music festivals.
+        Hey! Thank you for visiting my website. I am Chiemela, a U.S Army veteran and full stack developer. I am proficient with MERN stack, however, I learn different technologies and add them to my skill set. Outside of work, I enjoy playing chess, working out, and attending music concerts.
       </p>
       <a href="#contact" className="button button--flex center-button">
         <p>Say Hello</p>
