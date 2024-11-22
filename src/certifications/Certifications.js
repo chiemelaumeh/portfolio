217fa4a8320d931c5628974ac9165b9d675d6cc5
chiemelaumeh <chiemelaumeh1@gmail.com>
2024-11-22 00:35:47 -0600

    portfolio

--- src/certifications/Certifications.js
+++ src/certifications/Certifications.js
@@ -5,16 +5,18 @@ import "./certifications.css";
 
 // import Devops from "./Devops"
 import Csaa from "./Csaa";
+import Cka from "./Cka";
 
 const Certifications = () => {
   return (
     <section className="skills section-half mb-main" id="skills">
       <h2 className="section__title section__mb ">Certifications</h2>
 
-      {/* //to add certs, increase  grid-template-columns: repeat(1, 350px); of the cer__container class in certification.css file */}
+      {/* //to add certs, increase grid-template-columns: repeat(1, 350px); of the cert__container class in certification.css file */}
       <div className="cert__container certgrid ">
         <Csaa />
-        {/* <Csaa /> */}
+        <Cka />
+  
       </div>
     </section>
   );
---
+++ src/certifications/Cka.js
@@ -0,0 +1,24 @@
+import React from "react";
+import image1 from "../assets/cka.webp";
+
+
+
+const Cka = () => {
+  return (
+    <div>
+
+
+      <div className="div-img">
+ 
+        <div className="image-board">
+          <img src={image1} alt="" className="testimonial__img" />
+          <h3 className="skills__title">CKA
+
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cka;
